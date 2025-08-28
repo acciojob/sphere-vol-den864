751e6789cd7729a800c1bc039d0ecafcc1efb7ee
Dhaval Birade <69907022+den864@users.noreply.github.com>
2025-08-28 09:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function volume_sphere() {
     //Write your code here
-  
+	const r=Number(document.getElementById("radius").value);
+	if(isNaN(r) || r<0){
+		document.getElementById("volume").value='NaN';
+	}else{
+		let v=(4/3)*Math.PI*Math.pow(r,3);
+		document.getElementById("volume").value=v.toFixed(4);
+	}  
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
